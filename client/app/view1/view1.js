e446bf26a0435c9c51fe053f032c73843e83eef3
imears <maxwellmears@gmail.com>
2015-09-04 11:08:07 -0700

    Show success or error when messgages are sent

--- client/app/view1/view1.js
+++ client/app/view1/view1.js
@@ -11,15 +11,14 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Controller', ['$scope', '$http', function($scope, $http) {
+
+  var thisController = this;
+
   this.learnMore = function() {
     console.log("Learn more clicked!");
   };
 
   this.sendMessage = function() {
-    console.log("send MESSAGE clicked...")
-    console.log("$scope.sendToNumber", this.sendToNumber)
-    console.log("$scope.message", this.message)
-
     $http.post('http://5971dd36.ngrok.io/text-message', {
       sendToNumber: this.sendToNumber,
       message: this.message
@@ -27,26 +26,26 @@ angular.module('myApp.view1', ['ngRoute'])
     .then(function(response) {
       // this callback will be called asynchronously when the response is available
       console.log("success!", response);
+      thisController.success = true;
     }, function(response) {
       // called asynchronously if an error occurs or server returns response with an error status.
+      thisController.error = true;
       console.error(response);
     })
   };
 
   this.sendPhoneMessage = function() {
-    console.log("send PHONE message clicked...")
-    console.log("this.sendToNumber2", this.sendToNumber2)
-    console.log("this.phoneMessage", this.phoneMessage)
-
     $http.post('http://5971dd36.ngrok.io/phone-message', {
       sendToNumber: this.sendToNumber2,
       message: this.phoneMessage
     })
     .then(function(response) {
+      thisController.success2 = true;
       // this callback will be called asynchronously when the response is available
       console.log("success!", response);
     }, function(response) {
       // called asynchronously if an error occurs or server returns response with an error status.
+      thisController.error2 = true;
       console.error(response);
     })
   };
